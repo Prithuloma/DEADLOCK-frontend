1d2129cdcc62dde3a7e5691d562d712c5342aea6
Prithuloma <prithuloma19@gmail.com>
2025-11-05 21:26:15 +0530

    Fix: cleaned imports and added useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import { useState } from 'react';
 import { useState, useEffect } from 'react';
 import API_BASE_URL from './api';
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,7 +13,6 @@ function App() {
     .then(data => console.log("✅ Backend connected:", data))
     .catch(err => console.error("❌ Backend connection error:", err));
 }, []);
-
 
   return (
     <>
